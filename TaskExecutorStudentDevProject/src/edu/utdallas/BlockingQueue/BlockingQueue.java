ae1091de031128c3df747af0f6cb1745d7f23474
Tyler <tylervanlim@gmail.com>
2024-10-22 21:10:32 -0500

    Annotated code

--- TaskExecutorStudentDevProject/src/edu/utdallas/BlockingQueue/BlockingQueue.java
+++ TaskExecutorStudentDevProject/src/edu/utdallas/BlockingQueue/BlockingQueue.java
@@ -3,7 +3,7 @@ package edu.utdallas.BlockingQueue;
 import java.util.LinkedList; // Import the LinkedList class.
 import java.util.Queue; // Import the Queue class.
 
-public class BlockingQueue<T> // Generic class with type T.
+public class BlockingQueue<T> // BlockingQueue class with generic type T.
 {
     private final Queue<T> queue = new LinkedList<T>(); // Create a new queue object with generic type T.
     private final int queue_capacity; // The capacity of the queue.
@@ -13,9 +13,9 @@ public class BlockingQueue<T> // Generic class with type T.
         this.queue_capacity = capacity; // Set the capacity of the queue.
     }
 
-    public void put(T item) throws InterruptedException
+    public void put(T item) throws InterruptedException // Add an item to the queue.
     {
-        synchronized(this)
+        synchronized(this) // Synchronize the block of code to ensure thread safety, avoids race conditions.
         {
             while(queue.size() == queue_capacity) // If the queue is full, wait until there is space.
             {
@@ -26,9 +26,9 @@ public class BlockingQueue<T> // Generic class with type T.
         }
     }
 
-    public T take() throws InterruptedException
+    public T take() throws InterruptedException // Remove and return an item from the queue.
     {
-        synchronized(this)
+        synchronized(this) // Synchronize the block of code to ensure thread safety.
         {
             while(queue.isEmpty()) // If the queue is empty, wait until there is an item.
             {
--- TaskExecutorStudentDevProject/src/edu/utdallas/taskExecutorImpl/TaskExecutorImpl.java
+++ TaskExecutorStudentDevProject/src/edu/utdallas/taskExecutorImpl/TaskExecutorImpl.java
@@ -2,55 +2,56 @@ package edu.utdallas.taskExecutorImpl;
 
 import edu.utdallas.taskExecutor.Task;
 import edu.utdallas.taskExecutor.TaskExecutor;
-import edu.utdallas.BlockingQueue.BlockingQueue;
+import edu.utdallas.BlockingQueue.BlockingQueue; // Import the BlockingQueue class.
 
-public class TaskExecutorImpl implements TaskExecutor
+public class TaskExecutorImpl implements TaskExecutor // Implement the TaskExecutor interface.
 {
-    private final Thread[] threads;
-    private final BlockingQueue<Task> taskQueue;
+	// final as the threads and taskQueue are initialized in the constructor and will not be reassigned.
+    private final Thread[] threads; // Initialize an array of threads.
+    private final BlockingQueue<Task> taskQueue; // Initialize a BlockingQueue of tasks.
 
-    public TaskExecutorImpl(int threadPoolSize)
+    public TaskExecutorImpl(int threadPoolSize) // Constructor for the TaskExecutorImpl class.
     {
-        threads = new Thread[threadPoolSize];
-        taskQueue = new BlockingQueue<>(100); // Example capacity
+        threads = new Thread[threadPoolSize]; // Initialize the array of threads with the specified size.
+        taskQueue = new BlockingQueue<>(100); // Initialize the BlockingQueue with a capacity of 100.
 
         for (int i = 0; i < threadPoolSize; i++)
         {
-            threads[i] = new Thread(new TaskRunner());
-            threads[i].setName("TaskThread" + i);
-            threads[i].start();
+            threads[i] = new Thread(new TaskRunner()); // Create a new thread with a TaskRunner.
+            threads[i].setName("TaskThread" + i); // Set the name of the thread.
+            threads[i].start(); // Start the thread.
         }
     }
 
-    @Override
-    public void addTask(Task task)
+    @Override // Override the addTask method from the TaskExecutor interface.
+    public void addTask(Task task) // Add a task to the task queue.
     {
         try
         {
-            taskQueue.put(task); // Add the task to the blocking queue
+            taskQueue.put(task); // Add the task to the blocking queue.
         }
-        catch (InterruptedException e)
+        catch (InterruptedException e) // Catch InterruptedException if thrown.
         {
-            Thread.currentThread().interrupt(); // Restore interrupted status
+            Thread.currentThread().interrupt(); // Restore interrupted status.
         }
     }
 
-    private class TaskRunner implements Runnable
+    private class TaskRunner implements Runnable // Implement the Runnable interface for the TaskRunner class.
     {
-        @Override
+        @Override // Override the run method from the Runnable interface.
         public void run()
         {
-            while (true)
+            while (true) // Infinite loop to keep the thread running.
             {
                 try
                 {
-                    Task task = taskQueue.take(); // Take a task from the blocking queue
-                    task.execute(); // Execute the task
+                    Task task = taskQueue.take(); // Take a task from the blocking queue.
+                    task.execute(); // Task from the queue is executed.
                 }
                 catch (InterruptedException e)
                 {
-                    Thread.currentThread().interrupt(); // Restore interrupted status
-                    break; // Exit the loop if interrupted
+                    Thread.currentThread().interrupt(); // Restore interrupted status.
+                    break; // Exit the loop if interrupted.
                 }
             }
         }
