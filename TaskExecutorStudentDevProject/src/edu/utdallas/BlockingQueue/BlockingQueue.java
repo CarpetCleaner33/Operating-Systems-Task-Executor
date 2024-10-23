f6aa5dbf1bc4fe67f00bd240ae3e4db80d5c682c
Tyler <tylervanlim@gmail.com>
2024-10-22 20:57:57 -0500

    Finished TaskExecutorImpl and BlockingQueue

--- TaskExecutorStudentDevProject/src/edu/utdallas/BlockingQueue/BlockingQueue.java
+++ TaskExecutorStudentDevProject/src/edu/utdallas/BlockingQueue/BlockingQueue.java
@@ -3,13 +3,40 @@ package edu.utdallas.BlockingQueue;
 import java.util.LinkedList; // Import the LinkedList class.
 import java.util.Queue; // Import the Queue class.
 
-public class BlockingQueue
+public class BlockingQueue<T> // Generic class with type T.
 {
-    private final Queue<T> queue = new LinkedList<T>(); // Create a new queue object.
+    private final Queue<T> queue = new LinkedList<T>(); // Create a new queue object with generic type T.
     private final int queue_capacity; // The capacity of the queue.
 
     public BlockingQueue(int capacity) // Constructor for the BlockingQueue class.
     {
         this.queue_capacity = capacity; // Set the capacity of the queue.
+    }
+
+    public void put(T item) throws InterruptedException
+    {
+        synchronized(this)
+        {
+            while(queue.size() == queue_capacity) // If the queue is full, wait until there is space.
+            {
+                wait(); // Wait until there is space in the queue.
+            }
+            queue.add(item); // Add the item to the queue.
+            notifyAll(); // Notify all threads that are waiting.
+        }
+    }
+
+    public T take() throws InterruptedException
+    {
+        synchronized(this)
+        {
+            while(queue.isEmpty()) // If the queue is empty, wait until there is an item.
+            {
+                wait(); // Wait until there is an item in the queue.
+            }
+            T item = queue.remove(); // Remove and return the item from the queue.
+            notifyAll(); // Notify all threads that are waiting.
+            return item; // Return the item.
+        }
     }
 }
--- TaskExecutorStudentDevProject/src/edu/utdallas/taskExecutorImpl/TaskExecutorImpl.java
+++ TaskExecutorStudentDevProject/src/edu/utdallas/taskExecutorImpl/TaskExecutorImpl.java
@@ -2,32 +2,37 @@ package edu.utdallas.taskExecutorImpl;
 
 import edu.utdallas.taskExecutor.Task;
 import edu.utdallas.taskExecutor.TaskExecutor;
-import edu.utdallas.BlockingQueue.BlockingQueue; // Imports blocking queue implementation.
-
+import edu.utdallas.BlockingQueue.BlockingQueue;
 
 public class TaskExecutorImpl implements TaskExecutor
 {
-    private final Thread[] threads; // Final keyword is used to make sure that the threads array is not re-assigned.
-    
+    private final Thread[] threads;
+    private final BlockingQueue<Task> taskQueue;
+
     public TaskExecutorImpl(int threadPoolSize)
     {
-        //TODO Complete the implementation
-		// Initialize the thread pool
-        threads = new Thread[threadPoolSize]; // Create a new object array of threads with the size of the thread pool.
-        
-        for (int i = 0; i < threadPoolSize; i++) // Create a new instance of thread for each thread in the thread pool.
+        threads = new Thread[threadPoolSize];
+        taskQueue = new BlockingQueue<>(100); // Example capacity
+
+        for (int i = 0; i < threadPoolSize; i++)
         {
-            threads[i] = new Thread(new TaskRunner()); // New instance of TaskRunner is created and passed to the thread.
-            threads[i].setName("TaskThread" + i); // Set the name of the thread.
-            threads[i].start(); // Start the thread.
+            threads[i] = new Thread(new TaskRunner());
+            threads[i].setName("TaskThread" + i);
+            threads[i].start();
         }
     }
 
     @Override
     public void addTask(Task task)
     {
-        // TODO Complete the implementation
-		// Will be implemented once the blocking queue is ready
+        try
+        {
+            taskQueue.put(task); // Add the task to the blocking queue
+        }
+        catch (InterruptedException e)
+        {
+            Thread.currentThread().interrupt(); // Restore interrupted status
+        }
     }
 
     private class TaskRunner implements Runnable
@@ -37,20 +42,15 @@ public class TaskExecutorImpl implements TaskExecutor
         {
             while (true)
             {
-                // Retrieve and execute tasks from the blocking queue (to be implemented)
-                Task task = null; // This will be retrieved from the queue later
-                
                 try
                 {
-                    if (task != null)
-                    {
-                        task.execute();
-                    }
+                    Task task = taskQueue.take(); // Take a task from the blocking queue
+                    task.execute(); // Execute the task
                 }
-                catch (Exception e)
+                catch (InterruptedException e)
                 {
-                    // Handle exception and ensure the thread continues running
-                    System.err.println("Task execution error: " + e.getMessage());
+                    Thread.currentThread().interrupt(); // Restore interrupted status
+                    break; // Exit the loop if interrupted
                 }
             }
         }
